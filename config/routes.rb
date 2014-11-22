2fda7224d1ab8bd9d6b4165e5fa136a92d23751f
Adam Jacques <adam@adamjacques.com>
2014-11-21 22:50:21 -0500

    Moved to Piwik for metrics. Fixed send_mail broken route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ResumeWebsite::Application.routes.draw do
 	root :to => 'home#resume'
 	get 'home' => 'home#resume'
 	get 'contact_me' => 'contact#create_mail'
-	get 'send_mail' => 'contact#send_mail'
+	post 'send_mail' => 'contact#send_mail'
 	get 'key_geopoints' => 'geospatial#key_points'
 
 	scope 'admin' do
