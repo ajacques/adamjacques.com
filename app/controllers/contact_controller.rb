4edee0b1833f2d8ada8db991d4a7d860e1c386c9
Adam Jacques <adam@adamjacques.com>
2016-01-01 15:11:27 -0800

    Ruby 1.9.x upgrades

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,6 +7,6 @@ class ContactController < ApplicationController
     subject = Rails.application.appconfig.email_subject
     ContactMailer.user_contact(target, "website@#{request.host}", subject, params[:name], params[:email], params[:subject], request.remote_ip, params[:message]).deliver
 
-    redirect_to :controller => 'home', :action => 'resume'
+    redirect_to controller: 'home', action: 'resume'
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,13 @@
 class ContactMailer < ActionMailer::Base
 	def user_contact(target, from, email_subject, name, email, subject, ip, message)
-		@params = {:name => name, :email => email, :subject => subject, :message => message}
+		@params = {name: name, email: email, subject: subject, message: message}
 		@ipaddr = ip
 		@date = Time.now.utc
 
 		headers 'Reply-To' => email,
 				'X-Mailer' => 'Ruby'
 
-		mail :to => target, :from => from, :subject => email_subject do |format|
+		mail to: target, from: from, subject: email_subject do |format|
 			format.text
 		end
 	end
