3c0c81ad886212fb341d476d95c1e6522e06ecb2
Adam Jacques <adam@technowizardry.net>
2016-07-01 08:33:58 -0700

    Deprecation fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ ResumeWebsite::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
