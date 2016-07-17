23f910e2c8d9dcc2f25ef524b8e69c4f5a2a5717
Adam Jacques <adam@technowizardry.net>
2016-07-17 12:34:11 -0700

    Update beacon proxy routing

--- app/support/beacon_proxy.rb
+++ app/support/beacon_proxy.rb
@@ -3,7 +3,7 @@ require 'rack-proxy'
 class BeaconProxy < Rack::Proxy
   def rewrite_env(env)
     request = Rack::Request.new(env)
-    uri = URI.parse(Rails.application.appconfig.analytics['beacon_url'])
+    uri = URI.parse(Rails.application.config.analytics['beacon_url'])
     env['HTTP_HOST'] = uri.host
     env
   end
