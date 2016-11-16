a914002eff75eddd1c46b102261021d4bdb9cdd6
Adam Jacques <adam@technowizardry.net>
2016-11-15 18:50:09 -0800

    Foreign keys for everybody

---
+++ db/migrate/004_add_foreign_keys.rb
@@ -0,0 +1,17 @@
+class AddForeignKeys < ActiveRecord::Migration
+  def change
+    add_foreign_key :degrees, :education
+    add_foreign_key :educations, :organization
+    add_foreign_key :jobs, :location
+    add_foreign_key :jobs, :organization
+    add_foreign_key :jobs, :people, column: :manager
+    add_foreign_key :pay_rates, :job, on_delete: :cascade
+    add_foreign_key :descriptions, :job, on_delete: :cascade
+    add_foreign_key :employee_ids, :job, on_delete: :cascade
+    add_foreign_key :key_points, :key_points, column: :parent_id, on_delete: :cascade
+    add_foreign_key :people, :organization
+    add_foreign_key :projects, :organization
+    add_foreign_key :references, :people
+    add_foreign_key :references, :job
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "conferences", force: :cascade do |t|
     t.string  "name",        limit: 32, null: false
