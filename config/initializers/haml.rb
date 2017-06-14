bee539568066dcab63530be59af116f73efcfc86
Adam Jacques <adam@technowizardry.net>
2017-06-13 22:32:29 -0700

    Revert "Update HAML parser to new Ruby format"
    This reverts commit 5645363d2a52e37f8086c47c0e1ea2c8d7febcc9.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,14 +1,17 @@
-module HamlCompacter
-  def parse_tag(line)
-    result = super(line)
-    unless result.size == 9 && [false, true].include?(result[4]) && [false, true].include?(result[5])
-      raise "Unexpected parse_tag output: #{result.inspect}"
+# Override module that will strip all whitespace from the HTML in production
+module Haml
+  class Parser
+    def parse_tag_with_nuked_whitespace(line)
+      result = parse_tag_without_nuked_whitespace line
+      unless result.size == 9 && [false, true].include?(result[4]) && [false, true].include?(result[5])
+        raise "Unexpected parse_tag output: #{result.inspect}"
+      end
+      result[4] = true # nuke_outer_whitespace
+      result[5] = true # nuke_inner_whitespace
+      result
     end
-    result[4] = true # nuke_outer_whitespace
-    result[5] = true # nuke_inner_whitespace
-    result
+    alias_method_chain :parse_tag, :nuked_whitespace if ::Rails.env.production?
   end
 end
 
-Haml::Parser.send(:prepend, HamlCompacter) if ::Rails.env.production?
 Haml::Template.options[:attr_wrapper] = '"'
