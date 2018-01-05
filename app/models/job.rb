f0ca23fee4629050ebc08b31fbdce30a329e4f61
deppbot <deppbot@jollygoodcode.com>
2018-01-05 11:46:13 +0800

    Bundle Update on 2018-01-04 (#18)
    * Bundle Update on 2018-01-04
    
    * Rubocop

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ApplicationRecord
   # Associations
   belongs_to :location
   belongs_to :organization
-  has_many :responsibilities, class_name: 'Description'
+  has_many :responsibilities, class_name: 'Description', inverse_of: :job
 
   scope :visible, (-> { includes(:responsibilities, :location, :organization).order('start_date DESC').active })
   scope :active, (-> { where(active: true) })
