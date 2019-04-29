f88b4b9530014078f29eda0f68f5694731a0f59d
Adam Jacques <adam@technowizardry.net>
2019-04-28 23:08:41 -0700

    Add secret key for assets to fix build

--- config/environments/assets.rb
+++ config/environments/assets.rb
@@ -29,4 +29,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.secret_key_base = SecureRandom.hex(64)
 end
