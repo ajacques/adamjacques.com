6e286dc5fcad5c99cbdf1cd086311febed845b05
Adam Jacques <ajacques@users.noreply.github.com>
2021-09-04 21:00:16 -0700

    Blacklist tracing for the health check

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,25 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   config.traces_sample_rate = 0.5
+  config.traces_sampler = lambda do |context|
+    unless context[:parent_sampled].nil?
+      next context[:parent_sampled]
+    end
+
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /request/
+      case transaction_name
+      when 'health#ping'
+        0
+      else
+        0.5
+      end
+    else
+        0.5
+    end
+  end
 end
