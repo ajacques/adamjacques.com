a1e17451d86dddad72c472aa592169c22c3608be
Adam Jacques <ajacques@users.noreply.github.com>
2022-02-01 23:22:09 -0800

    Only instrument from upstream

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -3,7 +3,14 @@ require 'opentelemetry/exporter/otlp'
 require 'opentelemetry/instrumentation/mysql2'
 require 'opentelemetry/instrumentation/rails'
 
+instrumentation_config = {
+  'OpenTelemetry::Instrumentation::ActionPack' => { enable_recognize_route: true },
+}
+
 OpenTelemetry::SDK.configure do |c|
   c.service_name = 'ResumeWebsite'
-  c.use_all() # enables all instrumentation!
+  c.use_all(instrumentation_config)
 end
+
+# Parent is NGINX
+OpenTelemetry.tracer_provider.sampler = OpenTelemetry::SDK::Trace::Samplers.parent_or_else(:always_off)
